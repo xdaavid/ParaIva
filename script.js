21d99bb7260bd2fa835328a7b918e448995b51fe
xdaavid <118130625+xdaavid@users.noreply.github.com>
2024-02-15 00:27:09 -0500

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttonYes = document.querySelector('#yes')
 let fontSize = 2
 
 let messages = [
-  'Estas segurx?',
+  'Estas segura?',
   'Piensalo bien',
   'Piensalo muy bien',
   'Piensalo',
